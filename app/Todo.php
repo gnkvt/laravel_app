583001aa22b0aa09c4c6b536b56ca6575cb1fcf0
Gen Kubota <gen.kvt@gmail.com>
2020-01-07 19:00:47 +0900

    論理削除の実装

--- app/Todo.php
+++ app/Todo.php
@@ -3,11 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
 
     // protected $table = 'todos';
+
+    use SoftDeletes;
+
+
 
     protected $fillable = [
         'title',
